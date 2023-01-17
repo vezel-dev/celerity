86c6aa4ef7eb7c3ff5ccf201ff47d403f8ed2af5
Alex Rønne Petersen <alex@alexrp.com>
2023-01-17 16:54:20 +0100

    Fix a CS8625 error from BenchmarkDotNet update.

--- src/benchmarks/CelerityBenchmarkConfig.cs
+++ src/benchmarks/CelerityBenchmarkConfig.cs
@@ -16,7 +16,7 @@ internal sealed class CelerityBenchmarkConfig : ManualConfig
 
         _ = WithOptions(ConfigOptions.JoinSummary | ConfigOptions.StopOnFirstError)
             .WithArtifactsPath(Path.Combine(Path.GetDirectoryName(Environment.ProcessPath!)!, "artifacts"))
-            .WithSummaryStyle(new(CultureInfo.InvariantCulture, false, null, TimeUnit.Microsecond))
+            .WithSummaryStyle(new(CultureInfo.InvariantCulture, false, null!, TimeUnit.Microsecond))
             .AddAnalyser(
                 BaselineCustomAnalyzer.Default,
                 EnvironmentAnalyser.Default,
