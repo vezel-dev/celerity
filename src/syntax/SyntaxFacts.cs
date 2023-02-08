24406ba921f161fd226560ad7bf63deee2dc9791
Alex Rønne Petersen <alex@alexrp.com>
2023-02-08 10:37:08 +0100

    Add SyntaxFacts methods for checking characteristics of various SyntaxTokenKinds.

--- src/syntax/SyntaxFacts.cs
+++ src/syntax/SyntaxFacts.cs
@@ -79,4 +79,109 @@ public static class SyntaxFacts
             _ => null,
         };
     }
+
+    public static bool IsKeyword(SyntaxTokenKind kind)
+    {
+        Check.Enum(kind);
+
+        return kind switch
+        {
+            SyntaxTokenKind.AndKeyword or
+            SyntaxTokenKind.AsKeyword or
+            SyntaxTokenKind.AssertKeyword or
+            SyntaxTokenKind.BreakKeyword or
+            SyntaxTokenKind.CatchKeyword or
+            SyntaxTokenKind.CondKeyword or
+            SyntaxTokenKind.ConstKeyword or
+            SyntaxTokenKind.DeferKeyword or
+            SyntaxTokenKind.ElseKeyword or
+            SyntaxTokenKind.ErrKeyword or
+            SyntaxTokenKind.ExtKeyword or
+            SyntaxTokenKind.FalseKeyword or
+            SyntaxTokenKind.FnKeyword or
+            SyntaxTokenKind.IfKeyword or
+            SyntaxTokenKind.InKeyword or
+            SyntaxTokenKind.LetKeyword or
+            SyntaxTokenKind.MatchKeyword or
+            SyntaxTokenKind.ModKeyword or
+            SyntaxTokenKind.MutKeyword or
+            SyntaxTokenKind.NextKeyword or
+            SyntaxTokenKind.NilKeyword or
+            SyntaxTokenKind.NotKeyword or
+            SyntaxTokenKind.OpaqueKeyword or
+            SyntaxTokenKind.OrKeyword or
+            SyntaxTokenKind.PubKeyword or
+            SyntaxTokenKind.RaiseKeyword or
+            SyntaxTokenKind.RecKeyword or
+            SyntaxTokenKind.RecvKeyword or
+            SyntaxTokenKind.RetKeyword or
+            SyntaxTokenKind.TailKeyword or
+            SyntaxTokenKind.TestKeyword or
+            SyntaxTokenKind.TrueKeyword or
+            SyntaxTokenKind.TypeKeyword or
+            SyntaxTokenKind.UseKeyword or
+            SyntaxTokenKind.WhileKeyword or
+            SyntaxTokenKind.AgentKeyword or
+            SyntaxTokenKind.AnyKeyword or
+            SyntaxTokenKind.AtomKeyword or
+            SyntaxTokenKind.BoolKeyword or
+            SyntaxTokenKind.HandleKeyword or
+            SyntaxTokenKind.IntKeyword or
+            SyntaxTokenKind.NoneKeyword or
+            SyntaxTokenKind.RealKeyword or
+            SyntaxTokenKind.RefKeyword or
+            SyntaxTokenKind.StrKeyword or
+            SyntaxTokenKind.FriendKeyword or
+            SyntaxTokenKind.MacroKeyword or
+            SyntaxTokenKind.MetaKeyword or
+            SyntaxTokenKind.PragmaKeyword or
+            SyntaxTokenKind.QuoteKeyword or
+            SyntaxTokenKind.TryKeyword or
+            SyntaxTokenKind.UnquoteKeyword or
+            SyntaxTokenKind.WithKeyword or
+            SyntaxTokenKind.YieldKeyword => true,
+            _ => false,
+        };
+    }
+
+    public static bool IsOperator(SyntaxTokenKind kind)
+    {
+        Check.Enum(kind);
+
+        return kind switch
+        {
+            SyntaxTokenKind.BitwiseOperator or
+            SyntaxTokenKind.ShiftOperator or
+            SyntaxTokenKind.MultiplicativeOperator or
+            SyntaxTokenKind.AdditiveOperator => true,
+            _ => false,
+        };
+    }
+
+    public static bool IsIdentifier(SyntaxTokenKind kind)
+    {
+        Check.Enum(kind);
+
+        return kind switch
+        {
+            SyntaxTokenKind.UpperIdentifier or
+            SyntaxTokenKind.LowerIdentifier or
+            SyntaxTokenKind.DiscardIdentifier => true,
+            _ => false,
+        };
+    }
+
+    public static bool IsLiteral(SyntaxTokenKind kind)
+    {
+        Check.Enum(kind);
+
+        return kind switch
+        {
+            SyntaxTokenKind.IntegerLiteral or
+            SyntaxTokenKind.RealLiteral or
+            SyntaxTokenKind.AtomLiteral or
+            SyntaxTokenKind.StringLiteral => true,
+            _ => false,
+        };
+    }
 }
