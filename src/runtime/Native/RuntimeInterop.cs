3ba3a81b85840209041f5a3796c08ee0715e98d4
Alex Rønne Petersen <alex@alexrp.com>
2023-02-14 02:44:14 +0100

    Don't try to be overly clever in RuntimeInterop strings.
    It's ugly.

--- src/runtime/Native/RuntimeInterop.cs
+++ src/runtime/Native/RuntimeInterop.cs
@@ -4,14 +4,12 @@ internal static partial class RuntimeInterop
 {
     private const string Library = "Vezel.Celerity.Runtime.Native";
 
-    private const string Prefix = "celerity_";
-
     static RuntimeInterop()
     {
         Initialize();
     }
 
-    [LibraryImport(Library, EntryPoint = $"{Prefix}initialize")]
+    [LibraryImport(Library, EntryPoint = $"celerity_initialize")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static partial void Initialize();
 }
