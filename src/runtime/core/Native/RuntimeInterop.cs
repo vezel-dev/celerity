275c11472eaff92b9d94729a4c6aa6108a20c0c3
Alex Rønne Petersen <alex@alexrp.com>
2023-02-25 09:46:04 +0100

    Some restructuring of runtime projects.
    We will use the native library for all interop in the runtime. This allows us
    to disable runtime marshalling in all projects.
    
    Closes #8.

--- src/runtime/core/Native/RuntimeInterop.cs
+++ src/runtime/core/Native/RuntimeInterop.cs
@@ -1,15 +1,15 @@
 namespace Vezel.Celerity.Runtime.Compiler.Native;
 
-internal static partial class JitInterop
+internal static partial class RuntimeInterop
 {
     private const string Library = "Vezel.Celerity.Runtime.Native";
 
-    static JitInterop()
+    static RuntimeInterop()
     {
         Initialize();
     }
 
-    [LibraryImport(Library, EntryPoint = $"celerity_initialize")]
+    [LibraryImport(Library, EntryPoint = "celerity_initialize")]
     [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static partial void Initialize();
 }
