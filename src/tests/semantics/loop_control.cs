683c2e95a675f2fcab01ed6e0fe0a9b216cbfc60
Alex Rønne Petersen <alex@alexrp.com>
2023-03-19 13:51:41 +0100

    Add loop_control test.

---
+++ src/tests/semantics/loop_control.cs
@@ -0,0 +1,50 @@
+public sealed partial class SemanticTests
+{
+    [Fact]
+    public Task loop_control()
+    {
+        return TestAsync(
+            SyntaxMode.Module,
+            """
+            mod {
+                fn loops() {
+                    break;
+
+                    next;
+
+                    while true {
+                        break;
+
+                        break as 1;
+
+                        next;
+                    };
+
+                    while break {
+                        2;
+                    };
+
+                    while next {
+                        3;
+                    };
+
+                    for _ in [] {
+                        break;
+
+                        break as 4;
+
+                        next;
+                    };
+
+                    for _ in break {
+                        5;
+                    };
+
+                    for _ in next {
+                        6;
+                    };
+                }
+            }
+            """);
+    }
+}
