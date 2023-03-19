adb30c0674be6280c7396e7e367c3a11d942e23e
Alex Rønne Petersen <alex@alexrp.com>
2023-03-19 13:19:32 +0100

    Add more cases to the duplicate_symbols test.

--- src/tests/semantics/duplicate_symbols.cs
+++ src/tests/semantics/duplicate_symbols.cs
@@ -7,11 +7,23 @@ public sealed partial class SemanticTests
             SyntaxMode.Module,
             """
             mod {
+                use Aaa = Bbb;
+                use Aaa = Bbb;
+
                 fn foo() { 1; }
 
                 const foo = 2;
 
                 test foo { assert true; }
+
+                fn bar(aaa, aaa) {
+                    fn(bbb, bbb) -> 3;
+                }
+
+                fn baz() {
+                    let (x, x) = (4, 5);
+                    let (_x, _x) = (x, x);
+                }
             }
             """);
     }
