a8291e541dfd92ff5b65ba9536306b84c72d04e1
Alex Rønne Petersen <alex@alexrp.com>
2023-03-20 02:08:28 +0100

    Add missing_tokens test.

---
+++ src/tests/syntax/missing_tokens.cs
@@ -0,0 +1,17 @@
+public sealed partial class SyntaxTests
+{
+    [Fact]
+    public Task missing_tokens()
+    {
+        return TestAsync(
+            SyntaxMode.Module,
+            """
+            mod {
+                type x = -;
+
+                @foo
+                fn () { 1; }
+            }
+            """);
+    }
+}
