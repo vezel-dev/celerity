a6f41d21bb6295c76d4add8e4f51b7eb6cd0fd54
Alex Rønne Petersen <alex@alexrp.com>
2023-03-24 13:13:51 +0100

    Add uppercase_base_indicators test.

---
+++ src/tests/quality/uppercase_base_indicators.cs
@@ -0,0 +1,22 @@
+public sealed partial class QualityTests
+{
+    [Fact]
+    public Task uppercase_base_indicators()
+    {
+        return TestAsync(
+            SyntaxMode.Module,
+            """
+            mod {
+                fn foo() {
+                    0b101;
+                    0B101;
+                    0x101;
+                    0X101;
+                    0o101;
+                    0O101;
+                }
+            }
+            """,
+            UppercaseBaseIndicatorPass.Instance);
+    }
+}
